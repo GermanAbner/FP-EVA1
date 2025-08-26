e0c12060b2afd0ffa7a2a90be2ccfafea4d32943
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-08-26 15:50:33 -0700

    EVA_Practica2_25550584.java

---
+++ EVA1_Practica2_25550584/src/eva1_practica2_25550584/EVA1_Practica2_25550584.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practica2_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA1_Practica2_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("German Rangel"); // ; Significa fin de Linea
+        // Las "    " son cadenas de texto< DATOS
+        //Numeros Enteros
+        System.out.println(1000);
+        //NUMEROS DECIMALES
+        System.out.println(52.3);
+        //VALORES LOGICOS (VERDADERO O FALSO)
+        // true false
+        System.out.println(true);
+        //Caracteres Individuales
+        //Cosas Distintas
+        System.out.println('c'); //caracteres
+        System.out.println("c"); // Cadera de Texto
+    }
+    
+}
