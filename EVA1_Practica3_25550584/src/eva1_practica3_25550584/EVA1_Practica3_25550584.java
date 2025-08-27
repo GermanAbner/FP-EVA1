bba9ce5cf0f4a269f04095570aedf2f3c726e56e
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-08-27 15:52:39 -0700

    EVA1_Practica3_25550584
    Nombre del Estudiante

---
+++ EVA1_Practica3_25550584/src/eva1_practica3_25550584/EVA1_Practica3_25550584.java
@@ -0,0 +1,18 @@
+
+package eva1_practica3_25550584;
+
+public class EVA1_Practica3_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Nombre del Estudiante: ");
+        System.out.println("German Abner Rangel Luna");
+        System.out.print("Numero de Control: ");
+        System.out.println(25550584);
+        System.out.print("Edad: ");
+        System.out.println(18);
+    }
+    
+}
