40894fcc0848c73c261f025900d2ef6db8b24997
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-08-27 15:54:05 -0700

    EVA1_Practica4_25550584
    Empleo

---
+++ EVA1_Practica4_25550584/src/eva1_practica4_25550584/EVA1_Practica4_25550584.java
@@ -0,0 +1,17 @@
+
+package eva1_practica4_25550584;
+
+public class EVA1_Practica4_25550584 {
+
+    public static void main(String[] args) {
+        System.out.print("Empleo: ");
+        System.out.println("Servicio de Mostrador");
+        System.out.print("Salario: ");
+        System.out.println("$2,900 Semanales");
+        System.out.print("Horas Semanales: ");
+        System.out.println("35h");
+        System.out.print("Requiere Ingles? ");
+        System.out.println("true");
+    }
+    
+}
