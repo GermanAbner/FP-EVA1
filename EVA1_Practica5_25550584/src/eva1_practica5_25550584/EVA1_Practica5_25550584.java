ba68e9db86361f88a3f16b116df9cfe7e678e6e7
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-08-28 16:08:22 -0700

    EVA1_Practica5_25550584
    Declaracion/Asignar

---
+++ EVA1_Practica5_25550584/src/eva1_practica5_25550584/EVA1_Practica5_25550584.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practica5_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA1_Practica5_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //Declarar las Variables
+    //Tipo de Dato      NOMBRE DEL IDENTIFICADOR
+        String nombre;
+        int edad;
+        double salario;
+        //INICIALIZACION
+        //Identificador  = Valor;
+        //= --> operador de asignacion
+        nombre = "Geman Abner Rangel Luna";
+        edad = 18;
+        salario = 1.1;
+        //Edad = "Ruben"
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
