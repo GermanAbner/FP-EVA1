a14c5687799911e1f572988b38c2bf5622388deb
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-09-01 15:48:20 -0700

    EVA1_Practica7_25550584
    Practica echa sin flowgoritm, la relacionada con el area de un triangulo

---
+++ EVA1_Practica7_25550584/src/eva1_practica7_25550584/EVA1_Practica7_25550584.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practica7_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA1_Practica7_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double base;
+       double altura;
+       double area;
+       
+       base = 5;
+       altura = 9;
+       area = (base*altura)/2;
+       
+       System.out.print("El area de un triangulo cuya base es 5 y altura es 9 es = ");
+       System.out.print(area);
+       System.out.println();
+    }
+    
+}
