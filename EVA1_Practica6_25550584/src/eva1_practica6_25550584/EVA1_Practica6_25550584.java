15d017b518482ab0bbeb9e86df91ef2689322d3f
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-09-01 15:40:12 -0700

    EVA1_Practica6_25550584
    Operaciones

---
+++ EVA1_Practica6_25550584/src/eva1_practica6_25550584/EVA1_Practica6_25550584.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practica6_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA1_Practica6_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double area, p, radio;
+       // es lo mismo que poner cada variable independiente, me ahorro espacio loco sigale ahi
+       //Ahora anotas los valores de las variables estas son las 3 de arriba w echale coco
+       p = 3.1416;
+       radio = 10;
+       //ahora va la operacion que no c porq pusimos radio*radio si pudimos hacer un codigo mas comodo pero x seguimos con la practica <3
+       area = 3.1416*radio*radio;
+       //esta parte del codigo es la que va a imprimir osea enseñar los datos ya echos en texto jejeje  
+       System.out.print("El area de un circulo de radio = 10 es ");
+       System.out.print(area);               
+       // Esta linea de codigo es para que se vea mejor el resultado <33333
+       System.out.println();   
+          }
+    
+}
