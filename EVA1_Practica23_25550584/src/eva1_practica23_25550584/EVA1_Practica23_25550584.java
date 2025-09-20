801418fbfcebd5e899ba358b8e7c2fc519c0e4ba
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-09-19 21:54:14 -0700

    EVA1_Practica23_25550584

---
+++ EVA1_Practica23_25550584/src/eva1_practica23_25550584/EVA1_Practica23_25550584.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practica23_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA1_Practica23_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int temp;
+        double tempdecimal;
+        tempdecimal = 24.5;
+                temp = (int)tempdecimal;//IMPORTANTE MIJO OMG *****************************************
+                System.out.println(temp);
+                
+                String cade, mensaje;
+                mensaje = "La temperatura es ";
+                cade = mensaje + temp; // CONCATENACION OMGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+                System.out.println(cade);
+    }
+    
+}
+///////////////// OMGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+
+/// si jalo JAJAJAAJAJ
